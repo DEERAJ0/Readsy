aaeae71b5e495d470fad70fc56c3f15a9680d062
DEERAJ0 <133205608+DEERAJ0@users.noreply.github.com>
2025-07-07 12:18:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -365,7 +365,7 @@ app.post("/api/updateBook/:id", async (req, res) => {
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
-  app.get("*",(req,res)=>{
+  app.get("/*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
   })
 }
