25e97a920db58c0c968465e7b690d5e68c363419
DEERAJ0 <133205608+DEERAJ0@users.noreply.github.com>
2025-07-07 14:23:48 +0530

    Update bookStore.js

--- frontend/src/store/bookStore.js
+++ frontend/src/store/bookStore.js
@@ -2,7 +2,7 @@ import {create} from 'zustand';
 import axios from 'axios';
 
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://readsy-yqw0.onrender.com/api";
 axios.defaults.withCredentials=true;
 
 export const useBookStore = create((set)=>({
